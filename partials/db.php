409c7146bd83464bf42418af8f5c406e7d8a0813
aikuaivenchorr <inarilainen@gmail.com>
2017-11-25 12:32:04 +0100

    made folders

---
+++ partials/db.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "localhost";
+$username="root";
+$password ="";
+$pdo = new PDO(
+  "mysql:host=localhost;dbname=todo;charset=utf8",
+  "root",
+  ""
+);
+
+//handling of error messages
+$pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//defence against simulated queries
+$pdo -> setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+?>
