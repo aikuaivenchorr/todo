b165ae3522cd2ec0150a6de28e2e764502e88a5a
aikuaivenchorr <inarilainen@gmail.com>
2017-11-25 15:47:19 +0100

    write and read from database

--- index.php
+++ index.php
@@ -7,8 +7,12 @@
 
 
     foreach($todos as $todo){
-      require_once 'partials/todo_row.php';
-    } 
+        echo $todo["title"] . $todo["priority"] . $todo["completed"] . "<br>";
+//        foreach($todo as $onetodo){
+//            echo "<br>" . $onetodo;
+        }
+//      require_once 'partials/todo_row.php';
+    
 
 
 echo "<hr><br><h1>TO DO LIST</h1><br>";
@@ -18,17 +22,18 @@ echo "<hr><br><h1>TO DO LIST</h1><br>";
 
 ?>
 <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>">  
-  Title: <input type="text" name="title" value="">
+  Title: <input type="text" name="title" value="" required>
   <br><br>
-  Completed: <input type="number" max="1" name="completed" value="">
+  Completed: <input type="number" min="0" max="1" name="completed" value="0" required>
   <br><br>
-  Priority: <input type="number" name="priority" value="">
+  Priority: <input type="number" min="1" name="priority" value="" required>
   <br><br>
   <input type="submit" name="submit" value="Submit">  
 </form>
 
 <?php 
 require_once 'partials/insert_new_todo.php';
+  
 
 ?>
 
