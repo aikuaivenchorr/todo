0e0428422f3c7f637cdabc63ff17f0e803bbc276
aikuaivenchorr <inarilainen@gmail.com>
2017-12-16 00:16:21 +0100

    checks for double title

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 //Redirect with getting new data from database. Prevents redirect while editing todo title
 if(!empty($_REQUEST) && empty($_GET['flagEdit'])){
-  header('Refresh: 3; URL=index.php');
+  header('Refresh: 7; URL=index.php');
    // header('Location:' . $_SERVER['PHP_SELF']);
 
 }
@@ -29,46 +29,18 @@ if(!empty($_REQUEST) && empty($_GET['flagEdit'])){
   
  
 <?php
-    $flagEdit = false;
-    if(!empty($_GET['flagEdit'])){
-       $flagEdit  = $_GET['flagEdit'];
-    if($flagEdit == true){
-         $flagEdit = true;
-    }
-    else{
-      $flagEdit = false;
-    }
-    }
+    //depending on value invariabl flagEdit shows button with pencil or editable title in input field
+  require 'partials/show_title_or_edit_title.php';
+    
+    //initializing arrays for 2 lists of non completd and completed tasks
     $todoList = array();
     $doneList = array();
 
-?>
-    
-   
-    
-   <h1>TO DO LIST</h1>
-   
-  <h2>What to do: </h2>
-   <form name="todo_form" action="index.php" method="POST">
-        <fieldset>
-            <legend>Add new todo:</legend>
-               <label for="title">Title</label>
-               <input type="text" name="title" id="title">
-               
-               <label for="priority">Priority</label>
-               <input type="number" name="priority" id="priority">
-               <label for="createdBy">Author</label>
-               <input type="text" name="createdBy" value="Mariia"id="createdBy">
-               <input type="submit" value="submit">
-       </fieldset>
-   </form>
-   <br><br>
-   
-<?php 
+
     
      require 'partials/fetch_all_todos.php';
     
-    //  var_dump($todos);
+   
     
     //creates 2 array for not completed and completed todos
      foreach($todos as $todo){
@@ -85,6 +57,29 @@ if(!empty($_REQUEST) && empty($_GET['flagEdit'])){
          
 ?>
       
+    
+   
+    
+   <h1>TO DO LIST</h1>
+   
+  <h2>What to do: </h2>
+<!--  Adds new task to do -->
+   <form name="todo_form" action="index.php" method="POST">
+        <fieldset>
+            <legend>Add new todo:</legend>
+               <label for="title">Title</label>
+               <input type="text" name="title" id="title">
+               
+               <label for="priority">Priority</label>
+               <input type="number" name="priority" id="priority">
+               <label for="createdBy">Author</label>
+               <input type="text" name="createdBy" value="Mariia"id="createdBy">
+               <input type="submit" value="submit">
+       </fieldset>
+   </form>
+   <br><br>
+   
+
       
 <div id="not_completed">
 
@@ -95,7 +90,7 @@ if(!empty($_REQUEST) && empty($_GET['flagEdit'])){
     foreach($todoList as $todo){
             $id = $todo['id'];
             $title = $todo['title'];
-           // $editButtonCheck = '<i class="fas fa-check"></i>';
+           
             $editField = '<form name="form_edit" method="POST" action="' . $_SERVER['PHP_SELF'] . '">  
 
                     <input type=""  name="editTitle" value="' . $title . '">
@@ -121,6 +116,14 @@ if(!empty($_REQUEST) && empty($_GET['flagEdit'])){
 
                     <button type="submit" id="editFlagSubmit" name="flagSubmit"  title="Edit" value="editFlag"><i class="fas fa-pencil-alt"></i></button> 
                     </form> ' ;
+                   
+                   
+                echo  '<form name="form_completed" method="POST" action="' . $_SERVER['PHP_SELF'] . '"> <input type="hidden" value="' . $id . '" name="id" id="title">
+                  <input type="hidden" value="1" name="completed" id="completed">
+
+                  <input type="submit" value="Done!">
+
+                  </form> ' ;
                }
         else{
             echo $editField . " ";
@@ -128,17 +131,11 @@ if(!empty($_REQUEST) && empty($_GET['flagEdit'])){
                  
                   
                  
-                  echo  $todo['createdBy'] . 
+                //  echo  $todo['createdBy']; 
                 
 
-                  '<form name="form_completed" method="POST" action="' . $_SERVER['PHP_SELF'] . '"> <input type="hidden" value="' . $id . '" name="id" id="title">
-                  <input type="hidden" value="1" name="completed" id="completed">
 
-                  <input type="submit" value="Done!">
-
-                  </form> ' 
-
-                . "<hr>";
+              echo "<hr>";
 
     }
 ?>       
@@ -161,10 +158,11 @@ if(!empty($_REQUEST) && empty($_GET['flagEdit'])){
 
                   <button type="submit" id="delete" name="deleteSubmit"  title="Delete" value="delete"><i class="fas fa-times"></i></button> </form> ' 
                     
-                  . $todo['priority'] . " <strong>" 
-                  .  $todo['title'] . "</strong> " . 
-                    ' <i class="fas fa-check"></i> '
-                  .  $todo['createdBy'] . "<hr>";
+                  . $todo['priority'] . 
+                    " <strong> <strike>"  .  $todo['title'] . " </strike></strong> ";
+                 //   echo ' <i class="fas fa-check"></i> ';
+                // echo  $todo['createdBy'];
+                echo "<hr>";
 
             }
          
--- partials/actions.php
+++ partials/actions.php
@@ -4,18 +4,31 @@ $notification = "";
 
 if(!empty($_POST)){
     if(empty($_POST['completed']) && empty($_POST['deleteId'])  && empty($_POST['editTitle'])  ){
-        require 'partials/insert_new_todo.php';
-   
-       if($statement){
-          $published = "<br> Published!";
-          $notification = $published;
-           
+       $title = $_POST['title'];
+        $doubleTitle = false;
+        foreach($todoList as $todo){
+                if($todo['title'] ==  $title ){
+
+                     echo "Can't add 2 similar titles! Change name of todo, please!";
+                     $doubleTitle = true;
+                    break;
+                }
+
+            }
+        
+            if($doubleTitle != true){
+              require 'insert_new_todo.php';
+                  if($statement){
+                      $published = "<br> Published!";
+                      $notification = $published;
+                    }
         }
+
     
     }
 
     elseif(!empty($_POST["completed"]) && $_POST["completed"] == '1'){
-        require 'partials/check_as_done.php';
+        require 'check_as_done.php';
         
         if($statement){
            $done = "<br> Done!";
@@ -25,7 +38,7 @@ if(!empty($_POST)){
     }
     
     elseif(!empty($_POST['deleteId'])){
-        require 'partials/delete_todo.php';
+        require 'delete_todo.php';
         
         if($statement){
            $deleted = "<br> Deleted!";
@@ -35,7 +48,7 @@ if(!empty($_POST)){
     }
     
         elseif(!empty($_POST['editTitle'])){
-        require 'partials/edit_todo.php';
+        require 'edit_todo.php';
         
         if($statement){
            $edited = "<br> Edited!";
