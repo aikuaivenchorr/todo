29a28b5cf35704d9bc874c275b3646393f9ea23c
aikuaivenchorr <inarilainen@gmail.com>
2017-12-22 22:07:56 +0100

    style input fields

--- partials/add_new_task.php
+++ partials/add_new_task.php
@@ -4,7 +4,6 @@
             <legend>Add new todo:</legend>
                <label for="title">Title</label>
                <input type="text" name="title" id="title">
-               
                <label for="priority">Priority</label>
                <input type="number" name="priority" id="priority">
                <label for="createdBy">Author</label>
